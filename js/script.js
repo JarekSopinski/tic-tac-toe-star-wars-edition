f11d6383bc2b89c3a6b519cdaa142ec234f36bb9
JarekSopinski <jarek.sopinski@gmail.com>
2018-05-05 15:41:53 +0200

    Hiding settings after choosing side

---
+++ js/script.js
@@ -0,0 +1,23 @@
+const settings = $("#js-settings");
+const chooseCross = $("#js-choose-cross");
+const chooseCircle = $("#js-choose-circle");
+
+const board = $("#js-board");
+
+let state = {};
+
+const initialState = {
+
+};
+
+// ************** EVENT LISTENERS ****************** //
+
+const handleSettingsClick = (crossOrCircle) => {
+    console.log(crossOrCircle);
+    settings.toggleClass("hidden");
+    board.toggleClass("hidden")
+};
+
+chooseCross.on("click", () => handleSettingsClick("cross"));
+chooseCircle.on("click", () => handleSettingsClick("circle"));
+
