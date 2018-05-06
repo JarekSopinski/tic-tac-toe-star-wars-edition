b35dc02cec0ca7d38503e8c5070c09a818ee96db
JarekSopinski <jarek.sopinski@gmail.com>
2018-05-06 08:20:20 +0200

    Setting signs in game's state

--- js/script.js
+++ js/script.js
@@ -8,16 +8,44 @@ let state = {};
 
 const initialState = {
 
+    playerSign: null,
+    computerSign: null
+
+};
+
+// ************** GAME MECHANICS ****************** //
+
+const startGame = (playerSign) => {
+
+    state = $.extend(true, {}, initialState);
+    // TODO: after restarting keep signs settings from previous game;
+
+    // set signs for player and computer
+    state.playerSign = playerSign;
+    state.playerSign === "cross" ? state.computerSign = "circle" : state.computerSign = "cross";
+
+    // TODO: deciding who starts the game also should be handled here
+
 };
 
 // ************** EVENT LISTENERS ****************** //
 
-const handleSettingsClick = (crossOrCircle) => {
-    console.log(crossOrCircle);
+const handleSettingsClick = (playerSign) => {
+
+    // hide settings screen and show game board
     settings.toggleClass("hidden");
-    gameScreen.toggleClass("hidden")
+    gameScreen.toggleClass("hidden");
+
+    startGame(playerSign);
+
 };
 
-chooseCross.on("click", () => handleSettingsClick("cross"));
-chooseCircle.on("click", () => handleSettingsClick("circle"));
+$(document).ready(() => {
+
+    startGame("cross"); // TODO: this is for development only, remove it later! Normally game is started from handleSettingsClick()
+
+    chooseCross.on("click", () => handleSettingsClick("cross"));
+    chooseCircle.on("click", () => handleSettingsClick("circle"));
+
+});
 
